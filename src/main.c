76ae87d2deff988303b04e9c174fd4f4d8a8be0a
Sujay-korde <sujaykorde04@gmail.com>
2026-05-30 12:35:16 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -12,6 +12,6 @@ int main(int argc, char *argv[]) {
   fgets(input, 100, stdin);
 
   input[strlen(input) - 1] = '\0';
-  printf("%s: Command not found\n",input);
+  printf("%s: command not found\n",input);
   return 0;
 }
