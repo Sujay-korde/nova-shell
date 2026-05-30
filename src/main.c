8928779fe81304751c5a27611dbe12ff10f93aeb
Sujay-korde <sujaykorde04@gmail.com>
2026-05-30 12:39:48 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,12 +6,15 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
+  
+  while(1){
+    printf("$ ");
 
-  char input[100];
-  fgets(input, 100, stdin);
-
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n",input);
+    char input[100];
+    fgets(input, 100, stdin);
+  
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n",input);
+  }
   return 0;
 }
