5990f74b477460772a1389c669ae949b4c9af7f0
Sujay-korde <sujaykorde04@gmail.com>
2026-06-13 00:57:03 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -22,25 +22,70 @@ int main(int argc, char *argv[]) {
   
     input[strlen(input) - 1] = '\0';
 
-    if(strcmp(input, "exit") == 0){
+    // 1. TOKENIZE FIRST: Parse the command and arguments into an array right away
+    char *args[20]; 
+    int arg_count = 0;
+    
+    char *token = strtok(input, " ");
+    while (token != NULL && arg_count < 19) {
+      args[arg_count++] = token;
+      token = strtok(NULL, " ");
+    }
+    args[arg_count] = NULL; // The argument array must be NULL-terminated
+
+    // If the user just hits enter, skip processing and loop back
+    if (arg_count == 0) {
+      continue;
+    }
+
+    // 2. PROCESS COMMANDS USING THE UNIFORM args ARRAY
+    if(strcmp(args[0], "exit") == 0){
       break;
     }
-    else if(strncmp(input, "echo ", 5) == 0){
-      printf("%s\n", input + 5);
+    else if(strcmp(args[0], "echo") == 0){
+      // Print all arguments separated by spaces cleanly
+      for (int i = 1; i < arg_count; i++) {
+        printf("%s%s", args[i], (i == arg_count - 1) ? "" : " ");
+      }
+      printf("\n");
     }
-    else if(strncmp(input, "pwd", 3) == 0){
+    else if(strcmp(args[0], "pwd") == 0){
       char cwd[512];
       if(getcwd(cwd, sizeof(cwd)) != NULL){
-        printf("%s\n",cwd);
-      }else{
-        perror("getcwd() error");
+        printf("%s\n", cwd);
+      } else {
+        perror("pwd failed");
       }
     }
-    else if(strncmp(input, "type ", 5) == 0){
-      char* cmd = input + 5;
-      if(strcmp(cmd, "echo") == 0 || strcmp(cmd, "type") == 0 || strcmp(cmd, "exit") == 0 || strcmp(cmd, "pwd") == 0){
+    else if(strcmp(args[0], "cd") == 0){
+      if (arg_count < 2) {
+        // Fallback or default behavior if no path is given
+        char *home = getenv("HOME");
+        if (home) chdir(home);
+      } else {
+        char *target_dir = args[1];
+        
+        // Handle the "~" shorthand for home directory
+        if (strcmp(target_dir, "~") == 0) {
+          target_dir = getenv("HOME");
+        }
+        
+        if (chdir(target_dir) != 0) {
+          printf("cd: %s: No such file or directory\n", args[1]);
+        }
+      }
+    }
+    else if(strcmp(args[0], "type") == 0){
+      if (arg_count < 2) {
+        printf("type: missing operand\n");
+        continue;
+      }
+      
+      char* cmd = args[1];
+      if(strcmp(cmd, "echo") == 0 || strcmp(cmd, "type") == 0 || 
+         strcmp(cmd, "exit") == 0 || strcmp(cmd, "pwd") == 0 || strcmp(cmd, "cd") == 0){
         printf("%s is a shell builtin\n", cmd);
-      }else{
+      } else {
         char *path = getenv("PATH");
         if(!path){
           printf("%s: not found\n", cmd);
@@ -83,77 +128,61 @@ int main(int argc, char *argv[]) {
       }
     }
     else {
-      // --- NEW EXTERNAL PROGRAM EXECUTION LOGIC ---
-      
-      // 1. Parse the command and all of its arguments into an array
-      char *args[20]; 
-      int arg_count = 0;
-      
-      char *token = strtok(input, " ");
-      while (token != NULL && arg_count < 19) {
-        args[arg_count++] = token;
-        token = strtok(NULL, " ");
-      }
-      args[arg_count] = NULL; // The argument array must be NULL-terminated
+      // --- EXTERNAL PROGRAM EXECUTION LOGIC ---
+      char *cmd = args[0];
+      char *path = getenv("PATH");
+      char exec_path[512] = {0};
+      int found = 0;
 
-      if (arg_count > 0) {
-        char *cmd = args[0];
-        char *path = getenv("PATH");
-        char exec_path[512] = {0};
-        int found = 0;
-
-        if (path) {
-          char *copy = strdup(path);
-          if (copy) {
+      if (path) {
+        char *copy = strdup(path);
+        if (copy) {
 #ifdef _WIN32
-            char *delim = ";";
+          char *delim = ";";
 #else
-            char *delim = ":";
+          char *delim = ":";
 #endif
-            char *dir = strtok(copy, delim);
-            while (dir) {
+          char *dir = strtok(copy, delim);
+          while (dir) {
 #ifdef _WIN32
-                snprintf(exec_path, sizeof(exec_path), "%s\\%s.exe", dir, cmd);
+              snprintf(exec_path, sizeof(exec_path), "%s\\%s.exe", dir, cmd);
 #else
-                snprintf(exec_path, sizeof(exec_path), "%s/%s", dir, cmd);
+              snprintf(exec_path, sizeof(exec_path), "%s/%s", dir, cmd);
 #endif
-                if (access(exec_path, X_OK) == 0) {
-                    found = 1;
-                    break;
-                }
-                dir = strtok(NULL, delim);
-            }
-            free(copy);
+              if (access(exec_path, X_OK) == 0) {
+                  found = 1;
+                  break;
+              }
+              dir = strtok(NULL, delim);
           }
+          free(copy);
         }
+      }
 
-        // 2. If the executable is found in the PATH, run it
-        if (found) {
+      if (found) {
 #ifdef _WIN32
-          // Fallback message if run locally on a Windows shell environment
-          printf("Execution of external programs is only supported on Unix/Linux targets.\n");
+        printf("Execution of external programs is only supported on Unix/Linux targets.\n");
 #else
-          pid_t pid = fork();
-          
-          if (pid == 0) {
-            // Inside Child Process
-            execv(exec_path, args);
-            perror("execv failed");
-            exit(1);
-          } 
-          else if (pid > 0) {
-            // Inside Parent Process
-            int status;
-            waitpid(pid, &status, 0);
-          } 
-          else {
-            perror("fork failed");
-          }
-#endif
+        pid_t pid = fork();
+        
+        if (pid == 0) {
+          // Inside Child Process
+          execv(exec_path, args);
+          perror("execv failed");
+          exit(1);
+        } 
+        else if (pid > 0) {
+          // Inside Parent Process
+          int status;
+          waitpid(pid, &status, 0);
         } 
         else {
-          printf("%s: command not found\n", cmd);
+          perror("fork failed");
         }
+#endif
+      } 
+      else {
+        printf("%s: command not found\n", cmd);
       }
     }
   }
